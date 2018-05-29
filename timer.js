a51a86cffafdd8823229ac6f07c00df2cd03a0a3
Lee Jeong Chan <dlwjdcks7356@gmail.com>
2018-05-29 16:33:30 +0900

    Add game over event

--- timer.js
+++ timer.js
@@ -1,9 +1,15 @@
 var timer = document.getElementById("timer");
 var h = 0, m = 0, s = 0, ms = 0;
+var t;
 
 for(var i = 0; i < 25; i++) {
     if(document.getElementById(i + 1).innerHTML === "1") {
-        var index = i + 1;
+        var index_start = i + 1;
+    }
+    if(document.getElementById(i + 26).innerHTML === "50") {
+        var index_end = i + 26;
+    }
+    if(index_start !== undefined && index_end !== undefined) {
         break;
     }
 }
@@ -25,12 +31,17 @@ function setTime() {
         }
     }
 
-    timer.innerText = h + ":" + m + ":" + s + "." + ms;
+    timer.innerHTML = h + ":" + m + ":" + s + "." + ms;
     stopWatch();
 }
 
 function stopWatch() {
-    var t = setTimeout(setTime, 10);
+    t = setTimeout(setTime, 10);
 }
 
-document.getElementById(index).addEventListener('click', stopWatch);
+function done() {
+    clearTimeout(t);
+}
+
+document.getElementById(index_start).addEventListener('click', stopWatch);
+document.getElementById(index_end).addEventListener('click', done);
